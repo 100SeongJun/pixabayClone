099fd0b7a909766963ac0ada49825483c5a6ec19
백성준 <94966290+100SeongJun@users.noreply.github.com>
2023-01-08 00:19:26 +0900

    Remove Key

--- src/api/getdata.js
+++ src/api/getdata.js
@@ -1,7 +1,6 @@
 const url = {
   domain_name: "pixabay",
   url: "https://pixabay.com/api/",
-  key: "27684590-dd8981fd6d5ffb8ed54976303",
-  // key: "Pixabay api Key"
+//   key: "Pixabay api Key"
 };
 export default url;
